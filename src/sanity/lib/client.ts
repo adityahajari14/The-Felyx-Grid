b2ee0d50b9f005680d616b231e5d8c02bd7893b5
it-techmonkey <adityahazari14@gmail.com>
2026-05-15 13:50:14 +0530

    Updates

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -2,10 +2,13 @@ import { createClient } from "@sanity/client";
 
 import { apiVersion, dataset, projectId } from "../env";
 
+const token = process.env.SANITY_API_READ_TOKEN || process.env.SANITY_API_WRITE_TOKEN;
+
 export const client = createClient({
   apiVersion,
   dataset,
   projectId: projectId || "placeholder",
   perspective: "published",
+  token,
   useCdn: false,
 });
